2841df5524b91f24e7d9a065428a55347cb40374
Your Name <you@example.com>
2025-11-25 20:59:03 -0300

    subindo projeto

---
+++ scr/server.js
@@ -0,0 +1,39 @@
+mport express from 'express';
+import { Sequelize } from 'sequelize';
+
+const HOST = '127.0.0.1'
+const PORT = '5000'
+
+const app = express();
+
+// Middlewares
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('API de Produtos funcionando 🚀');
+})
+
+const sequelize = new Sequelize(
+    'postgresql://neondb_owner:npg_wfPBFJ47cndZ@ep-autumn-boat-a4nk7int-pooler.us-east-1.aws.neon.tech/planteidb?sslmode=require&channel_binding=require',
+    {
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true
+            }
+        },
+        loggin: true
+    }
+) // Example for postgres
+
+try {
+    await sequelize.authenticate()
+    console.log('🟢 Conectado ao banco de dados!');
+} catch (error) {
+    console.error('🔴 Erro ao conectar no banco:', error);
+}
+
+//Iniciando o server
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://${HOST}:${PORT}`);
+})
